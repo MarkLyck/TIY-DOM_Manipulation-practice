5714f12d29a1a4df295042a1c352dd814a1710e9
Mark Lyck <mark.lyck@gmail.com>
2016-07-04 21:47:59 -0500

    Updated assignment to use higher functions and ES6 methods

--- main.js
+++ main.js
@@ -1,131 +1,94 @@
-var $farmers = $('#farmers');
-var $cats = $('#cats');
 
-var farmersWithin10Miles = [];
+// Messed around with some ES6 stuff, to try to do this with a smaller amount of code.
+// Before: 131 lines, 3294 characters
+// After: 89 lines, 1933 Characters
 
-farmersData.forEach(function(farmer){
-  var distance = farmer.marketname.split(' ')[0];
+let $farmers = $('#farmers')
+let $cats = $('#cats')
+
+let farmersWithin10Miles = farmersData.filter((farmer) => {
+  let distance = farmer.marketname.split(' ')[0]
   if (Number(distance) <= 10) {
-    var farmerName = farmer.marketname.split(' ');
-    farmerName.shift();
-    var farmerString = farmerName.join(' ');
-    farmersWithin10Miles.push(farmerString);
+    return true;
   }
+})
+.forEach((farmer) => {
+  let farmerName = farmer.marketname.split(' ')
+  farmerName.shift()
+  let farmerString = farmerName.join(' ')
+  let liHTML = '<li>' + farmerString + '</li>'
+  let $li = $(liHTML)
+  $farmers.append($li)
 });
 
-farmersWithin10Miles.forEach(function(farmer){
-  var liHTML = '<li>' + farmer + '</li>';
-  var $li = $(liHTML);
-  $farmers.append($li);
-});
+cats.data.forEach((cat) => {
+  let liHTML = '<li><img src="' + cat.images.fixed_height.url + '"></li>'
+  let $li = $(liHTML)
+  $cats.append($li)
+})
 
-cats.data.forEach(function(cat){
-  // console.log(cat.images.fixed_height.url);
-  var liHTML = '<li><img src="' + cat.images.fixed_height.url + '"></li>';
-  var $li = $(liHTML);
-  $cats.append($li);
-});
+let $beers = $('#beers')
+let $berryBtn = $('.berry')
+let $apricotsBtn = $('.apricot')
+let $barrelAgedBtn = $('.barrel-aged')
 
-var $beers = $('#beers');
-var $berryBtn = $('.berry');
-var $apricotsBtn = $('.apricot');
-var $barrelAgedBtn = $('.barrel-aged');
+$berryBtn.on('click',() => {
+  $beers.empty()
+  filterBeerBy('berry')
+  filterBeerBy('berries')
+})
 
-//Berries
-$berryBtn.on('click',function(){
-  var beerList = [];
-  $beers.empty();
-  beers.data.forEach(function(beer){
+$apricotsBtn.on('click',() => {
+  $beers.empty()
+  filterBeerBy('apricot')
+})
+
+$barrelAgedBtn.on('click',() => {
+  $beers.empty()
+  filterBeerBy('barrel-aged')
+})
+
+let filterBeerBy = (filter) => {
+  let beerList = beers.data.filter((beer) => {
     if(beer.description !== undefined) {
-      if (beer.description.indexOf('berry') !== -1 || beer.description.indexOf('berries') !== -1) {
-
-        beerList.push(beer.name);
+      if (beer.description.indexOf(filter) !== -1) {
+        return true
+      } else if (beer.name.indexOf(filter) !== -1) {
+        return true
       }
     }
-    if(beer.description !== undefined) {
-      if (beer.description.indexOf('berry') !== -1 || beer.description.indexOf('berries') !== -1) {
-        if (beerList.indexOf(beer.name) === -1) {
-          beerList.push(beer.name);
-        }
-      }
-    }
-  });
-  beerList.forEach(function(beer){
-    var liHTML = '<li>' + beer + '</li>';
-    var $li = $(liHTML);
-    $beers.append($li);
-  });
-});
-// Apricots
-$apricotsBtn.on('click',function(){
-  var beerList = [];
-  $beers.empty();
-  beers.data.forEach(function(beer){
-    if(beer.description !== undefined) {
-      if (beer.description.indexOf('apricot') !== -1) {
-
-        beerList.push(beer.name);
-      }
-    }
-    if(beer.description !== undefined) {
-      if (beer.description.indexOf('apricot') !== -1) {
-        if (beerList.indexOf(beer.name) === -1) {
-          beerList.push(beer.name);
-        }
-      }
-    }
-  });
-  beerList.forEach(function(beer){
-    var liHTML = '<li>' + beer + '</li>';
-    var $li = $(liHTML);
-    $beers.append($li);
-  });
-});
-
-$barrelAgedBtn.on('click',function(){
-  var beerList = [];
-  $beers.empty();
-  beers.data.forEach(function(beer){
-    if(beer.description !== undefined) {
-      if (beer.description.indexOf('barrel-aged') !== -1) {
-
-        beerList.push(beer.name);
-      }
-    }
-    if(beer.description !== undefined) {
-      if (beer.description.indexOf('barrel-aged') !== -1) {
-        if (beerList.indexOf(beer.name) === -1) {
-          beerList.push(beer.name);
-        }
-      }
-    }
-  });
-  beerList.forEach(function(beer){
-    var liHTML = '<li>' + beer + '</li>';
-    var $li = $(liHTML);
-    $beers.append($li);
-  });
-});
-
-var $abvs = $('#ABVS');
-var abvObj = {};
-
-beers.data.forEach(function(beer){
-  var abv = 0;
-  if (beer.abv !== undefined) {
-    if (abvObj[Math.round(beer.abv)] === undefined) {
-      abvObj[Math.round(beer.abv)] = 1;
-    } else {
-      abvObj[Math.round(beer.abv)]++;
-    }
-  }
-});
-
-for (var abv in abvObj) {
-  console.log(abv + ' : ' + abvObj[abv]);
-  var trHTML = '<tr><td>' + abv + '</td><td>' + abvObj[abv] +'</td></tr>';
-  var $tr = $(trHTML);
-  $abvs.append($tr);
+  })
+  .map((beer) => {
+    return beer.name
+  })
+  .forEach((beer) => {
+    let liHTML = '<li>' + beer + '</li>'
+    let $li = $(liHTML)
+    $beers.append($li)
+  })
 }
 
-// console.log(abvObj);
+let $abvs = $('#ABVS')
+let abvObj = beers.data.reduce((obj, beer) => {
+  if (beer.abv !== undefined) {
+    if (obj[Math.round(beer.abv)] !== undefined) {
+      obj[Math.round(beer.abv)]++
+    } else {
+      obj[Math.round(beer.abv)] = 1
+    }
+    return obj
+  } else {
+    if (obj.unspecified !== undefined) {
+      obj.unspecified++
+    } else {
+      obj.unspecified = 1
+    }
+    return obj
+  }
+}, {})
+
+for (let abv in abvObj) {
+  let trHTML = '<tr><td>' + abv + '</td><td>' + abvObj[abv] +'</td></tr>'
+  let $tr = $(trHTML)
+  $abvs.append($tr)
+}
